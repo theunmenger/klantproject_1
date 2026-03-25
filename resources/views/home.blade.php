aa76c904c33477968d3c0c9acd8d19e7ebab5dbb
theunmenger <theun2005@hotmail.com>
2026-03-25 10:01:45 +0100

    structure update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<script
-    src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
-</script>
-<body>
+<x-layout>
+    <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
+    </script>
     <div id="main_container">
         <div class="graph_container">
             <h2 class="graph_title">Aantal werknemers ICT in Flevoland</h2>
@@ -27,5 +19,4 @@
         </div>     
     </div>
     <script src="js/odata_graph.js"></script>
-</body>
-</html>
+</x-layout>
